f8d1d92ecd8d9d9a28920b1a4a302b0df5f0fb89
Anchit <anchit.na@gmail.com>
2018-05-01 11:22:39 +0530

    Placement Application
    PlacementAutomation

---
+++ src/Placement/Global.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Placement;
+
+public class Global{
+   public static String jbtem;
+    public static String pswdem;
+    public static String jbtap;
+    public static String pswdap;   
+    public static String password;
+    public static int age;
+    public static String sex;
+    public static String branch;
+    public static String job_title;
+    public static String name;
+    public static int run;
+    public static String empname;
+    public static int running;
+    public static String oname;
+    public static String osex;
+    public static int oage;
+    public static String ojobtitle;
+    public static String obranch;
+    public static int vacancy;
+    public static int empty;
+     public static int flag;
+     public static int empty1;
+     public static int flag1;
+   
+}
+ 
